a7909371dde6fe3c6a36e121975c8d50641710b7
guismeiram <boralagui@outlook.com>
2022-07-01 02:34:28 -0300

    commitando a camda model

---
+++ src/main/java/com/example/jpql_cs/JpqlCsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpql_cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpqlCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpqlCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpql_cs/JpqlCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpql_cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
