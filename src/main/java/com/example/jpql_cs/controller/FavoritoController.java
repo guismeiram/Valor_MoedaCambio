db9cca0a2bb516ac66bb5b6944d58c813070c8fe
guismeiram <boralagui@outlook.com>
2022-07-01 14:34:01 -0300

    commitando controllers

--- src/main/java/com/example/jpql_cs/controller/FavoritoController.java
+++ src/main/java/com/example/jpql_cs/controller/FavoritoController.java
@@ -1,9 +1,22 @@
 package com.example.jpql_cs.controller;
 
+import com.example.jpql_cs.service.FavoritaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FavoritoController {
+    @Autowired
+    private FavoritaService favoritaService;
 
+    @Autowired
+    private final ModelMapper modelMapper;
 
+    public FavoritoController(ModelMapper modelMapper, FavoritaService favoritaService) {
+        this.modelMapper = modelMapper;
+        this.favoritaService = favoritaService;
+    }
+
+    
 }
--- src/main/java/com/example/jpql_cs/controller/NoticiaController.java
+++ src/main/java/com/example/jpql_cs/controller/NoticiaController.java
@@ -1,7 +1,20 @@
 package com.example.jpql_cs.controller;
 
+import com.example.jpql_cs.service.NoticiaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NoticiaController {
+    @Autowired
+    private NoticiaService noticiaService;
+
+    @Autowired
+    private final ModelMapper modelMapper;
+
+    public NoticiaController(ModelMapper modelMapper, NoticiaService noticiaService) {
+        this.modelMapper = modelMapper;
+        this.noticiaService = noticiaService;
+    }
 }
