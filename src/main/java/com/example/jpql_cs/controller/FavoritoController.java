ef1fde366658cf1bbd75aea668f648e825987154
guismeiram <boralagui@outlook.com>
2022-07-01 15:08:55 -0300

    commitando controllers

--- src/main/java/com/example/jpql_cs/controller/FavoritoController.java
+++ src/main/java/com/example/jpql_cs/controller/FavoritoController.java
@@ -1,22 +1,46 @@
 package com.example.jpql_cs.controller;
 
+import com.example.jpql_cs.dto.FavoritaDTO;
+import com.example.jpql_cs.model.Favorita;
 import com.example.jpql_cs.service.FavoritaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.transaction.Transactional;
+
+@RestController("/api/favoritos")
 public class FavoritoController {
     @Autowired
     private FavoritaService favoritaService;
 
     @Autowired
-    private final ModelMapper modelMapper;
+    private final ModelMapper mapper;
 
-    public FavoritoController(ModelMapper modelMapper, FavoritaService favoritaService) {
-        this.modelMapper = modelMapper;
+    public FavoritoController(ModelMapper mapper, FavoritaService favoritaService) {
+        this.mapper = mapper;
         this.favoritaService = favoritaService;
     }
 
-    
+    @PostMapping
+    @Transactional
+    public FavoritaDTO createConsulta(@RequestBody FavoritaDTO favoritaDTO)
+    {
+        Favorita favoritaRequest = mapper.map(favoritaDTO, Favorita.class);
+        //favorito
+        favoritaRequest.getCotacao().forEach(f ->f.setFavorita(f.getFavorita()));
+        favoritaRequest.getCreated();
+        favoritaRequest.getUpdated();
+        favoritaRequest.getNoticias();
+
+
+
+        //Grava na Memoria
+         Favorita favorita = favoritaService.createFavorita(favoritaRequest);
+        // convert entity to DTO
+        return mapper.map(favorita, FavoritaDTO.class);
+    }
+
 }
--- src/main/java/com/example/jpql_cs/controller/NoticiaController.java
+++ src/main/java/com/example/jpql_cs/controller/NoticiaController.java
@@ -1,9 +1,15 @@
 package com.example.jpql_cs.controller;
 
+import com.example.jpql_cs.dto.NoticiaDTO;
+import com.example.jpql_cs.model.Noticias;
 import com.example.jpql_cs.service.NoticiaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
 
 @RestController
 public class NoticiaController {
@@ -11,10 +17,30 @@ public class NoticiaController {
     private NoticiaService noticiaService;
 
     @Autowired
-    private final ModelMapper modelMapper;
+    private final ModelMapper mapper;
 
-    public NoticiaController(ModelMapper modelMapper, NoticiaService noticiaService) {
-        this.modelMapper = modelMapper;
+    public NoticiaController(ModelMapper mapper, NoticiaService noticiaService) {
+        this.mapper = mapper;
         this.noticiaService = noticiaService;
     }
+
+    @PostMapping
+    @Transactional
+    public NoticiaDTO createConsulta(@RequestBody  NoticiaDTO  noticiaDTO)
+    {
+        Noticias noticiasRequest = mapper.map(noticiaDTO, Noticias.class);
+        //noticias
+        noticiasRequest.getFavorita().getUpdated();
+        noticiasRequest.getFavorita().getCreated();
+        noticiasRequest.getFavorita().getCotacao();
+        noticiasRequest.getDescricao();
+        noticiasRequest.getNoticia();
+
+        //Grava na Memoria
+        Noticias noticia = noticiaService.createNoticias(noticiasRequest);
+        // convert entity to DTO
+        return mapper.map(noticia, NoticiaDTO.class);
+    }
+
+    
 }
